f0fe5affcaa4520ecb2d9454af692f9db7a3dcf7
HaiDang <dangtran0710@gmail.com>
2023-05-05 11:26:26 +0700

    update DictServicesImpl

--- SoapDictionary/SoapDictionary/src/com/ws/DictServicesImpl.java
+++ SoapDictionary/SoapDictionary/src/com/ws/DictServicesImpl.java
@@ -12,24 +12,31 @@ public class DictServicesImpl implements DictServices{
 	@Override
 	public String dictENtoVn(String word) {
 		String result = null;
-		
-		try {
-			Connection conn = JDBCUtil.getConnection();
-			
-			String sql = "SELECT * FROM dict Where word = ?"; 
-			PreparedStatement pstm = conn.prepareStatement(sql);
-			pstm.setString(1, word);
-						
-			ResultSet rs = pstm.executeQuery();
-			
-			while (rs.next()) {				 
-				result=word.toUpperCase()  + ": " + rs.getString("mean");
-			}
-			conn.close();
-			pstm.close();
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
-		return result;
+	    try {
+	        Connection conn = JDBCUtil.getConnection();
+	        String sql = "SELECT * FROM dict WHERE word = ? OR mean = ?"; 
+	        PreparedStatement pstm = conn.prepareStatement(sql);
+	        pstm.setString(1, word);
+	        pstm.setString(2, word); // nếu input không phải là từ mà là nghĩa, thì sẽ tìm các từ có nghĩa này
+	        
+	        ResultSet rs = pstm.executeQuery();
+	        
+	        while (rs.next()) {                 
+	            String foundWord = rs.getString("word");
+	            String foundMean = rs.getString("mean");
+	            // Kiểm tra đầu vào là từ hay nghĩa và in ra tương ứng
+	            if (word.equalsIgnoreCase(foundWord)) {
+	                result = foundMean;
+	            } else if (word.equalsIgnoreCase(foundMean)) {
+	                result = foundWord;
+	            }
+	        }
+	        
+	        conn.close();
+	        pstm.close();
+	    } catch (SQLException e) {
+	        e.printStackTrace();
+	    }
+	    return result;
 	}
 }
